534a7f92184210b76954d2df3b8044fd80cbcb7e
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2015-01-15 08:02:21 +0000

    Replace GitHub banner with a footer link, as it was obscuring the settings button at some widths

--- app.jsx
+++ app.jsx
@@ -182,10 +182,7 @@ var DadJokes = React.createClass({
       </h1>}
       <footer>
         <a href="https://github.com/insin/just-dadjokes">
-          <img
-            style={{position: 'absolute', top: 0, right: 0, border: 0}}
-            src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"
-          />
+          Source on GitHub
         </a>
       </footer>
     </div>
